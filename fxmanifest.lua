3754dc7c761393d76d1baab14de5da70996b4b03
Peter <20475516+SirOMGitsYOU@users.noreply.github.com>
2025-07-27 18:44:34 +0100

    Upload map assets

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+Author 'Poitor'
+Description 'Reworked Legion Carpark'
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+files {
+    'stream/**.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
+
+this_is_a_map 'yes'
