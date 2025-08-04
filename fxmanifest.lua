8e8cfc7a611ff7647667586adec08fc63b5c13c0
Peter <20475516+SirOMGitsYOU@users.noreply.github.com>
2025-08-04 22:24:41 +0100

    Updates!
    - Redo FXManifest
    - Updated LOD (finally)
    - Redone the ymap as a whole

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,21 @@
-Author 'Poitor'
-Description 'Reworked Legion Carpark'
-
 fx_version 'cerulean'
 games { 'gta5' }
-
-files {
-    'stream/**.ytyp',
-}
-
-data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
+lua54 "yes"
 
 this_is_a_map 'yes'
+
+files {
+    'stream/ytyp/*.ytyp',
+    'stream/ydr/*.ydr',
+    'stream/ydd/*.ydd',
+    'stream/ymap/*.ymap',
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/ytyp/*.ytyp"
+
+escrowignore {
+    'stream/ytyp/*.ytyp',
+    'stream/ydr/*.ydr',
+    'stream/ydd/*.ydd',
+    'stream/ymap/*.ymap',
+}
